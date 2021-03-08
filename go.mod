be52d9c5bcab0192bd0f451ad5d94858e94b2362
liangyy <fouronefive415>
2021-03-09 07:31:54 +0800

    feat: init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/liangyuyi/version
+
+go 1.13
---
+++ output.go
@@ -0,0 +1,7 @@
+package version
+
+import "fmt"
+
+func Output(name string) string {
+	return fmt.Sprintf("hello %s, this is version 1")
+}
